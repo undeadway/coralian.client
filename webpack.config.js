693a6c345b849a7be637068cc59cc9dacd96244a
undeadway <undeadway@163.com>
2020-01-19 16:21:48 +0800

    修改 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var path = require("path");
 
 module.exports = {
-	entry: { "coralian": "./coralian/index.js" },
+	entry: { "coralian.client": "./src/index.js" },
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "[name].js"
 	},
-	mode: 'production'
+	mode: 'development'
 };
